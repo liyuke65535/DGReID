093786242cd08941465e742496ffd3abc8068b39
Liyuke <1323642423@qq.com>
2023-03-30 15:07:33 +0800

    update

--- model/backbones/mix_style_algos/domainmix.py
+++ model/backbones/mix_style_algos/domainmix.py
@@ -24,8 +24,9 @@ class DomainMix(nn.Module):
         self.loss = nn.SoftMarginLoss()
 
     def forward(self, x, labels, domain=None):
+        device = x.device
         if not self.training:
-            return x, 0.0
+            return x, torch.tensor(0.0, device=device)
         
         moment = self.momentum
         eps = self.eps
@@ -47,14 +48,14 @@ class DomainMix(nn.Module):
         self.num_batch = self.num_batch + 1
 
         if random.random() > self.p:
-            return input, 0.0
+            return input, torch.tensor(0.0, device=device)
         
         B = x.size(0)
         lmda = self.beta.sample((B, 1, 1))
-        lmda = lmda.to(x.device)
+        lmda = lmda.to(device)
         #### make sure that inds are all different from domain
         d_ind = random.choices(range(1, self.num_domains), k=B)
-        d_ind = torch.tensor(d_ind, device=domain.device) + domain
+        d_ind = torch.tensor(d_ind, device=device) + domain
         domain_select = d_ind % self.num_domains
 
         # ############ efdmix like #############
@@ -90,9 +91,9 @@ class DomainMix(nn.Module):
         hp = x_mix.mean(1) #### or x_new
         feat_expand = torch.cat([x.mean(1), hp, hg],dim=0)
         N = feat_expand.size(0)
-        labels_new = torch.cat([labels,labels,-torch.ones([N-2*B], dtype=labels.dtype, device=labels.device)], dim=0)
+        labels_new = torch.cat([labels,labels,-torch.ones([N-2*B], dtype=labels.dtype, device=device)], dim=0)
         # x_expand = torch.cat([x_expand, hg], dim=0).view(3*B, -1)
-        # labels_new = torch.cat([labels_new, -torch.ones([B], dtype=labels.dtype, device=labels.device)], dim=0)
+        # labels_new = torch.cat([labels_new, -torch.ones([B], dtype=labels.dtype, device=device)], dim=0)
         dist_mat = euclidean_dist(feat_expand, feat_expand)
         assert len(dist_mat.size()) == 2
         assert dist_mat.size(0) == dist_mat.size(1)
