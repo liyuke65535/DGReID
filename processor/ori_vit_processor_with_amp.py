552fd08fe718a710c50ebee688b2539ee17f0f7a
Liyuke <1323642423@qq.com>
2023-04-09 19:38:50 +0800

    update

--- processor/ori_vit_processor_with_amp.py
+++ processor/ori_vit_processor_with_amp.py
@@ -162,7 +162,9 @@ def ori_vit_do_train_with_amp(cfg,
                 # loss_tri = torch.tensor(0.0, device=device)
 
                 #### scatter loss
-                loss_sct = domain_SCT_loss(feat, t_domains)
+                # styles = torch.arange(16).repeat(4).to(device)
+                # loss_sct = domain_SCT_loss(feat, styles)
+                loss_sct = torch.tensor(0.0, device=device)
 
                 #### center loss
                 if 'center' in cfg.MODEL.METRIC_LOSS_TYPE:
