5dfb5a5f4511bab1695e067277c34899e6904d77
liyuke875 <1323642423@qq.com>
2023-04-20 20:59:55 +0800

    update

--- model/backbones/prompt_vit.py
+++ model/backbones/prompt_vit.py
@@ -223,7 +223,7 @@ class mix_vit(nn.Module):
         #     ])
         # self.domainmix = DomainMix(embed_dim, num_domains)
         self.domainqueue = nn.ModuleList([
-            DomainQueue(embed_dim, num_domains) for _ in range(3)
+            DomainQueue(embed_dim, num_domains) for _ in range(1)
             ])
 
         self.blocks = nn.ModuleList([
