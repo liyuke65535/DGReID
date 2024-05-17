1a08e42eb6d8d16b2efb25dca52ecb90ae8bf0fd
liyuke12580 <liyuke65535@gmail.com>
2024-05-17 01:23:30 +0000

    updates

--- data/datasets/market1501.py
+++ data/datasets/market1501.py
@@ -81,6 +81,106 @@ class Market1501(ImageDataset):
             data.append((img_path, pid, camid))
 
         return data
+    
+
+
+@DATASET_REGISTRY.register()
+class Market1501_gen(ImageDataset):
+    """Market1501.
+
+    Reference:
+        Zheng et al. Scalable Person Re-identification: A Benchmark. ICCV 2015.
+
+    URL: `<http://www.liangzheng.org/Project/project_reid.html>`_
+
+    Dataset statistics:
+        - identities: 1501 (+1 for background).
+        - images: 12936 (train) + 3368 (query) + 15913 (gallery).
+    """
+    _junk_pids = [0, -1]
+    dataset_dir = 'market1501'
+    dataset_url = 'http://188.138.127.15:81/Datasets/Market-1501-v15.09.15.zip'
+    dataset_name = "market1501"
+
+    def __init__(self, root='datasets', market1501_500k=False, **kwargs):
+        # self.root = osp.abspath(osp.expanduser(root))
+        self.root = root
+        self.dataset_dir = osp.join(self.root, self.dataset_dir)
+
+        # allow alternative directory structure
+        self.data_dir = self.dataset_dir
+        data_dir = osp.join(self.data_dir, 'Market1501')
+        if osp.isdir(data_dir):
+            self.data_dir = data_dir
+        else:
+            warnings.warn('The current data structure is deprecated. Please '
+                          'put data folders such as "bounding_box_train" under '
+                          '"Market-1501-v15.09.15".')
+
+        self.train_dir = osp.join(self.data_dir, 'bounding_box_train')
+        self.query_dir = osp.join(self.data_dir, 'query')
+        self.gallery_dir = osp.join(self.data_dir, 'bounding_box_test')
+        self.extra_gallery_dir = osp.join(self.data_dir, 'images')
+        self.market1501_500k = market1501_500k
+
+        required_files = [
+            self.data_dir,
+            self.train_dir,
+            self.query_dir,
+            self.gallery_dir,
+        ]
+        if self.market1501_500k:
+            required_files.append(self.extra_gallery_dir)
+        self.check_before_run(required_files)
+
+        train = self.process_dir(self.train_dir)
+        train += self.process_dir_gen("/home/liyuke/data5/datasets/market1501_cp/bounding_box_train_gen") ### extra generated data
+        # pids_before = set()
+        # pids_after = set()
+        # for obj in train:
+        #     pids_before.add(obj[1])
+        # for obj in train_extra:
+        #     pids_after.add(obj[1])
+        query = self.process_dir(self.query_dir, is_train=False)
+        gallery = self.process_dir(self.gallery_dir, is_train=False)
+        if self.market1501_500k:
+            gallery += self.process_dir(self.extra_gallery_dir, is_train=False)
+
+        super(Market1501_gen, self).__init__(train, query, gallery, **kwargs)
+
+    def process_dir(self, dir_path, is_train=True):
+        img_paths = glob.glob(osp.join(dir_path, '*.jpg'))
+        pattern = re.compile(r'([-\d]+)_c(\d)')
+
+        data = []
+        for img_path in img_paths:
+            pid, camid = map(int, pattern.search(img_path).groups())
+            if pid == -1:
+                continue  # junk images are just ignored
+            assert 0 <= pid <= 1501  # pid == 0 means background
+            assert 1 <= camid <= 6
+            camid -= 1  # index starts from 0
+            if is_train:
+                pid = self.dataset_name + "_" + str(pid)
+            data.append((img_path, pid, camid))
+
+        return data
+    
+    def process_dir_gen(self, dir_path, is_train=True):
+        img_paths = glob.glob(osp.join(dir_path, '*.png'))
+        pattern = re.compile(r'([-\d]+)_gen')
+
+        data = []
+        for img_path in img_paths:
+            pid = int(pattern.search(img_path).groups()[0])
+            if pid == -1:
+                continue  # junk images are just ignored
+            assert 0 <= pid <= 1501  # pid == 0 means background
+            if is_train:
+                pid = self.dataset_name + "_" + str(pid)
+            data.append((img_path, pid, -1)) ## camid=-1, represent generated image
+        return data
+    
 
 
 @DATASET_REGISTRY.register()
